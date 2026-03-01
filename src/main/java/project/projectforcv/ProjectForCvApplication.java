58e5a8b71c66437228dffd45dc99568fa5c0e820
YanislavD <jnislav.dragiiski02@gmail.com>
2026-03-01 21:13:50 +0100

    Repo init

---
+++ src/main/java/project/projectforcv/ProjectForCvApplication.java
@@ -0,0 +1,13 @@
+package project.projectforcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForCvApplication.class, args);
+    }
+
+}

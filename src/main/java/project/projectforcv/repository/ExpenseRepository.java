ccfb244a2ef6bc7a76d0ab93ea3e96c68a852d15
YanislavD <jnislav.dragiiski02@gmail.com>
2026-03-02 21:07:29 +0100

    add ExpenseRepository

--- src/main/java/project/projectforcv/repository/ExpenseRepository.java
+++ src/main/java/project/projectforcv/repository/ExpenseRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.projectforcv.model.Expense;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
 
-
+    List<Expense> findByUserId(Long userId);
 }
